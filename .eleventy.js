0e554819329f6de743cbfe9195f424e73527bf4b
Atharva Kunte <athu.kunte@gmail.com>
2021-08-06 21:57:09 +0530

    Initial Commit : WEBDEV BLOG
    Signed-off-by: Atharva Kunte <athu.kunte@gmail.com>

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const{ DateTime } = require('luxon');
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("./src/main.css");
+    eleventyConfig.addPassthroughCopy("./src/media");
+
+    eleventyConfig.addFilter("postDate",(dateobj)=>{
+        return DateTime.fromJSDate(dateobj).toLocaleString(DateTime.DATE_MED);
+    } );
+    return{
+        dir:{
+            input:"src",
+            output:"public"
+        }
+    }
+}
