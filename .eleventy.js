522557e7067e59260a0692a7affa39579c7d01d7
Atharva Kunte <athu.kunte@gmail.com>
2021-08-06 23:02:49 +0530

    fix config
    Signed-off-by: Atharva Kunte <athu.kunte@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/main.css");
     eleventyConfig.addPassthroughCopy("./src/media");
+    eleventyConfig.addPassthroughCopy("./src/admin");
 
     eleventyConfig.addFilter("postDate",(dateobj)=>{
         return DateTime.fromJSDate(dateobj).toLocaleString(DateTime.DATE_MED);
